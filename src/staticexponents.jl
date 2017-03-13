b88ac4bd72f0b1024d16b2accec1572fab737048
Robin Deits <robin.deits@gmail.com>
2017-03-12 22:40:31 -0400

    WIP: experiment with Power type

--- src/staticexponents.jl
+++ src/staticexponents.jl
@@ -5,23 +5,47 @@ import Base: *, +, ^, convert, promote_rule, convert, show, isless
 abstract type PolynomialLike end
 abstract type TermLike <: PolynomialLike end
 abstract type MonomialLike <: TermLike end
-abstract type VariableLike <: MonomialLike end
+abstract type PowerLike <: MonomialLike end
+abstract type VariableLike <: PowerLike end
 
 immutable Variable{Name} <: VariableLike
 end
 
 isless(v1::Variable{N1}, v2::Variable{N2}) where {N1, N2} = isless(N1, N2)
-
 show(io::IO, v::Variable{Name}) where Name = print(io, Name)
+
+immutable Power{Var} <: PowerLike
+    exponent::Rational{Int}
+end
+
+variable(::Power{V}) where {V} = V
+exponent(p::Power) = p.exponent
+isless(p1::Power{V1}, p2::Power{V2}) where {V1, V2} = (V1, p1.exponent) < (V2, p2.exponent)
+
+function show(io::IO, p::Power)
+    print(io, variable(p))
+    d = denominator(exponent(p))
+    n = numerator(exponent(p))
+    if d == 1
+        if n != 1
+            print(io, "^", n)
+        end
+    else
+        print(io, "^(", exponent(p), ")")
+    end
+end
+
+convert(::Type{<:Power}, v::Variable) = Power{v}(1)
 
 immutable Monomial{Powers} <: MonomialLike
 end
 
-convert(::Type{<:Monomial}, v::Variable) = Monomial{((v, 1),)}()
+convert(::Type{<:Monomial}, p::Power) = Monomial{(p,)}()
+convert(T::Type{<:Monomial}, p::VariableLike) = convert(T, Power(p))
 
 function show(io::IO, m::Monomial{P}) where {P}
     for power in P
-        print(io, power[1], "^", power[2])
+        show(io, power)
     end
 end
 
@@ -31,8 +55,8 @@ immutable Term{Mono, T} <: TermLike
     Term{Mono, T}(c::T) where{Mono, T} = new{Mono, T}(c)
 end
 
-convert(::Type{<:Term}, mono::M) where {M <: Monomial} = Term{M()}(1)
-convert(T::Type{<:Term}, v::Variable) = convert(T, Monomial(v))
+convert(::Type{<:Term}, mono::M) where {M <: Monomial} = Term{mono}(1)
+convert(T::Type{<:Term}, v::PowerLike) = convert(T, Monomial(v))
 
 function show(io::IO, t::Term{Mono, T}) where {Mono, T}
     print(io, t.coefficient, Mono)
@@ -56,35 +80,46 @@ function show(io::IO, p::Polynomial)
     end
 end
 
-(^)(v::Variable, ::Type{Val{x}}) where x = Monomial{((v, x),)}()
-(*)(v1::Variable{Name}, v2::Variable{Name}) where Name = Monomial{((v1, 2),)}()
-@generated function (*)(v1::Variable{N1}, v2::Variable{N2}) where {N1, N2}
-    @assert N1 != N2
-    if N1 < N2
-        quote
-            Monomial{((v1, 1), (v2, 1))}()
-        end
-    else
-        quote
-            Monomial{((v2, 1), (v1, 1))}()
-        end
-    end
-end
-
-@generated function promote_rule(::Type{Term{Monomial{P1}, T}}, ::Type{Term{Monomial{P2}, T}}) where {T, P1, P2}
-    vars = Set{Any}()
-    for v in P1
-        push!(vars, v)
-    end
-    for v in P2
-        push!(vars, v)
-    end
-    vars = Tuple(sort(collect(vars)))
+@generated function (^)(v::V, ::Val{x}) where {V <: Variable, x}
     quote
-        Term{Monomial{$(vars)}(), T}
+        Monomial{$((Power{V()}(x),))}()
     end
 end
 
+@generated function (^)(v::V, ::Type{Val{x}}) where {V <: Variable, x}
+    quote
+        Monomial{$((Power{V()}(x),))}()
+    end
+end
+
+@generated function (*)(v1::V, v2::V) where {V <: Variable}
+    quote
+        Monomial{$((Power{V()}(2),))}()
+    end
+end
+
+@generated function (*)(v1::V1, v2::V2) where {V1 <: Variable, V2 <: Variable}
+    @assert V1 != V2
+    p = Tuple(sort([Power{V1()}(1), Power{V2()}(1)]))
+    quote
+        Monomial{$(p)}()
+    end
+end
+
+# @generated function promote_rule(::Type{Term{Monomial{P1}, T}}, ::Type{Term{Monomial{P2}, T}}) where {T, P1, P2}
+#     vars = Set{Any}()
+#     for v in P1
+#         push!(vars, v)
+#     end
+#     for v in P2
+#         push!(vars, v)
+#     end
+#     vars = Tuple(sort(collect(vars)))
+#     quote
+#         Term{Monomial{$(vars)}(), T}
+#     end
+# end
+#
 powers(::Monomial{P}) where {P} = P
 powers(::Type{Term{M, T}}) where {M, T} = powers(M)
 
@@ -118,7 +153,7 @@ end
     for power in powers(M2)
         i = indexin([power], p)[1]
         if i != 0
-            p[i] = (p[i][1], p[i][2] + power[2])
+            p[i] = Power{variable(p[i])}(exponent(p[i]) + exponent(power))
         else
             push!(p, power)
         end
@@ -130,14 +165,13 @@ end
 
 (+)(t1::PolynomialLike, t2::PolynomialLike) = convert(Polynomial, t1) + convert(Polynomial, t2)
 (+)(t1::Term{P, T1}, t2::Term{P, T2}) where {P, T1, T2} = Term{P}(t1.coefficient + t2.coefficient)
-(+)(m1::MonomialLike, m2::MonomialLike) = Term(m1) + Term(m2)
-# (+)(v1::Variable, v2::Variable) = Term(Monomial(v1)) + Term(Monomial(v2))
+(+)(v1::MonomialLike, v2::MonomialLike) = Term(v1) + Term(v2)
 (*)(x::Number, t::T) where {T <: Term} = T(x * t.coefficient)
 (*)(t::T, x::Number) where {T <: Term} = T(t.coefficient * x)
 (*)(x::Number, m::MonomialLike) = x * Term(m)
 (*)(m::MonomialLike, x::Number) = Term(m) * x
 (*)(t1::TermLike, t2::TermLike) = Term(t1) * Term(t2)
-# (*)(x::Number, m::Monomial) = x * Term(m)
+(*)(x::Number, m::Monomial) = x * Term(m)
 
 
 end
